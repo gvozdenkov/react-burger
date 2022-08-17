4007932c9328e982d589ca4a8f14b52188210e47
arty gvozdenkov <gvozdenkov@gmail.com>
2022-08-17 17:38:46 +0300

    feat: Header comp basic layout

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,7 +1,35 @@
+import {
+  BurgerIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import React from "react";
 
 function AppHeader() {
-  return <header className="header page__section">AppHeader</header>;
+  return (
+    <header className="header page__section pt-4 pb-4">
+      <nav>
+        <ul className="nav">
+          <li className="nav__item">
+            <BurgerIcon type="primary" />
+            <p className="nav__item-text nav__item-text_active">Конструктор</p>
+          </li>
+          <li className="nav__item nav__item_list">
+            <ListIcon type="secondary" />
+            <p className="nav__item-text">Лента заказов</p>
+          </li>
+          <li className="nav__item">
+            <Logo />
+          </li>
+          <li className="nav__item nav__item_profile">
+            <ProfileIcon type="secondary    " />
+            <p className="nav__item-text">Личный кабинет</p>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default AppHeader;
